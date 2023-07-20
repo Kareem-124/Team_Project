dd6d5bc9185d575bf11a5bb17c7fbe557309257f
Yazan Muqbel <muqbel.yazan@gmail.com>
2023-07-20 14:45:31 +0300

    Update- Homepage + Dashboard added inside Django
    Update by Yazan July 20- 300PM

--- StockMaster/StockMaster_app/urls.py
+++ StockMaster/StockMaster_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('signup/', views.signup_page, name='signup-page'),
+    path('login/', views.signin_page, name='signin-page'),
 ]
--- StockMaster/StockMaster_app/views.py
+++ StockMaster/StockMaster_app/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# This function renders the homepage
 def homepage(request):
     return render(request, 'homepage.html')
+
+#This function renders the sign up page upon button click
+def signup_page(request):
+    return render(request, 'pages-register.html')
+
+#This function renders the Log In page upon button click
+def signin_page(request):
+    return render(request, 'dashboard.html')
