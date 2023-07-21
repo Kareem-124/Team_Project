229792e12911904e28637377fef14ab1f8e65d9c
Yazan Muqbel <muqbel.yazan@gmail.com>
2023-07-21 23:23:57 +0300

    July 21- changes to sign up pages

--- StockMaster/StockMaster_app/models.py
+++ StockMaster/StockMaster_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import re 
 
-# Create your models here.
+class UserManager(models.Manager):
+    def regValidator(self, postData):
+        errors = {}
+        #input validations
+        if len(postData['f_name']) < 2:
+            errors['f_name'] = "First Name should at least be 2 charecters"
+        if len(postData['l_name']) < 2:
+            errors['l_name'] = "Last Name should at least be 2 charecters"
+        if len(postData['s_name']) < 2:
+            errors['s_name'] = "Store Name should at least be 2 charecters"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"
+        if User.objects.filter(email=postData['email']).exists():
+            errors['email'] = "This email is already registered!"
+        if len(postData['password'] or postData['password_conf']) < 8:
+            errors['password_len'] = "Password should atleast be 8 charecters"
+        if postData['password'] != postData['password_conf']:
+            errors['password_match'] = "Passwords do not matchx"
+        return errors 
+
+    def loginValidator(self, postData):
+        errors = {}  
+        if not (User.objects.filter(email=postData['email']) and User.objects.filter(password=postData['password'])):
+            errors['login'] = "Login failed! Check email and password"
+
+        return errors 
+
+class User(models.Model):
+    f_name = models.CharField(max_length=255)
+    l_name = models.CharField(max_length=255)
+    s_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager() 
--- StockMaster/StockMaster_app/views.py
+++ StockMaster/StockMaster_app/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import * 
+from django.contrib import messages
 
-# Create your views here.
 
 # This function renders the homepage
 def homepage(request):
     return render(request, 'homepage.html')
-
-#This function renders the sign up page upon button click
-def signup_page(request):
-    return render(request, 'pages-register.html')
-
-#This function renders the Log In page upon button click
-def signin_page(request):
-    return render(request, 'dashboard.html')
 
 #This function renders the User Profile page upon button click
 def profile(request):
@@ -25,3 +18,40 @@ def dashboard(request):
 #This function renders the page that displays all products in the database
 def products(request):
     return render(request, 'products-page.html')
+
+################## Registration and Loging ################
+#This function renders the sign up page upon button click
+def signup_page(request):
+    return render(request, 'pages-register_2.html')
+
+#This function for registration process
+def register(request):
+    errors = User.objects.regValidator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/signup')
+    else:
+        User.objects.create(
+        f_name= request.POST['f_name'], 
+        l_name= request.POST['l_name'], 
+        s_name= request.POST['s_name'], 
+        email=request.POST['email'], 
+        password=request.POST['password'])
+        return render(request, 'pages-login.html')
+
+#This function renders the Log In page upon button click
+def signin_page(request):
+    return render(request, 'pages-login.html')
+#THis function for loging process
+def login(request):
+    errors = User.objects.loginValidator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return render(request, 'pages-login.html')
+    else:
+        user = User.objects.get(email = request.POST['email'])
+        request.session['user'] = user.id
+        request.session['username'] = user.f_name
+        return redirect('/dashboard')
