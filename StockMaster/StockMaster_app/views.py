de234fd8669b59bbf9582a5a8343d7c37d559d3b
Kareem Malhis <kareem-124@hotmail.com>
2023-07-22 22:34:22 +0300

    Ajax

--- StockMaster/StockMaster_app/views.py
+++ StockMaster/StockMaster_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, reverse
-from .models import *
-from .models import Prodcut 
+from django.shortcuts import render, redirect
+from .models import * 
 from django.contrib import messages
+from django.http import JsonResponse
+from django.core import serializers
 
 
 # This function renders the homepage
@@ -14,11 +15,7 @@ def profile(request):
 
 #This function renders the dashboard after the user logs in to his acoount
 def dashboard(request):
-    context = {
-    'username' :  request.session['user'],
-    'user' : User.objects.get(id=request.session['user'])
-    }
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html')
 
 #This function renders the page that displays all products in the database
 def products(request):
@@ -27,7 +24,7 @@ def products(request):
 ################## Registration and Loging ################
 #This function renders the sign up page upon button click
 def signup_page(request):
-    return render(request, 'pages-register2.html')
+    return render(request, 'pages-register.html')
 
 #This function for registration process
 def register(request):
@@ -48,8 +45,7 @@ def register(request):
 #This function renders the Log In page upon button click
 def signin_page(request):
     return render(request, 'pages-login.html')
-
-#This function for loging process
+#THis function for loging process
 def login(request):
     errors = User.objects.loginValidator(request.POST)
     if len(errors) > 0:
@@ -66,22 +62,19 @@ def login(request):
 def order_page(request):
     return render(request,'orders_page.html')
 
-#This function renders the "add new product" form
-def add_prodcut(request):
-    return render(request, 'add_product.html')
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
-#This function handles POST data from "add new product" form and adds new PRODUCT to db:
-def save_product(request):
-    if request.method == 'POST':
-        params = dict()
-        
-        params['p_name'] = request.POST.get('p_name')
-        params['p_barcode'] = request.POST.get('p_barcode')
-        params['expire_date'] = request.POST.get('expire_date')
-        params['cost'] = request.POST.get('cost')
-        params['sale_price'] = request.POST.get('sale_price')
-        params['qty'] = request.POST.get('qty')
-        
-        Product.objects.create(**params)
-
-    return redirect(reverse('products-page'))
+#Process: Order list
+def order_list_process(request):
+    if is_ajax(request = request) and request.method == "POST":
+        # ADD Validation
+        product = Prodcuts.objects.get(p_barcode=request.POST['barcode'])
+        order_list_qty = request.POST['product_qty'] 
+        order_list_price = request.POST['product_price'] 
+        Order_list.objects.create(p_price=order_list_price,
+                                qty_sell=order_list_qty,
+                                product=product)
+        return JsonResponse({'message': 'Success'})
+    else:
+        return JsonResponse({'message': 'Invalid request'})
