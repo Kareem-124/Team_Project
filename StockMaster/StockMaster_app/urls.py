c321307811cc820985c46feb52ae98cd78763467
Kareem Malhis <kareem-124@hotmail.com>
2023-07-24 11:14:19 +0300

    upload_Process_Ordder+Clear_all+User_Session

--- StockMaster/StockMaster_app/urls.py
+++ StockMaster/StockMaster_app/urls.py
@@ -12,10 +12,16 @@ urlpatterns = [
     path('dashboard/products', views.products, name='products-page'),
     path('register', views.register, name='registrProcess'),
     path('loging', views.login, name="loginProcess"),
-    #Kareem urls
+    #Kareem urls Update1
     path('order_page', views.order_page, name="order_page"),
     path('order_list_process', views.order_list_process, name="order_list_process"),
     path('get_order_list', views.get_order_list, name="get_order_list"),
     path('remove_order_list/<int:order_id>', views.remove_order_list, name="remove_order_list"),
+    #Kareem urls Update2
+    path('process_order', views.process_order, name="process_order"),
+    path('clear_all_order_list_process', views.clear_all_order_list_process, name="clear_all_order_list_process"),
+    path('logout_process', views.logout_process, name="logout_process"),
+
 ]
+
     
--- StockMaster/StockMaster_app/views.py
+++ StockMaster/StockMaster_app/views.py
@@ -55,13 +55,19 @@ def login(request):
         return render(request, 'pages-login.html')
     else:
         user = User.objects.get(email = request.POST['email'])
+        print(user.id )
+        print("I added a user to the session")
         request.session['user'] = user.id
         request.session['username'] = user.f_name
         return redirect('/dashboard')
     
 #Page : Order Page
 def order_page(request):
-    return render(request,'orders_page.html')
+    user = check_session(request)
+    context = {
+        'user' : user
+    }
+    return render(request,'orders_page.html',context)
 
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
@@ -92,5 +98,38 @@ def remove_order_list(request,order_id):
     order_list.delete()
     return JsonResponse({'message': 'Success'})
 
+#----Update 2 -----------------------
+# Process: process_order
+def process_order(request):
+    # Get the objects from the order_list
+    order_list = Order_list.objects.all()
+    # Add the objects in the order_list to the order Table
+    for order in order_list:
+        Order.objects.create(p_price = order.p_price, qty_sell = order.qty_sell, products = order.products)
+    # Delete the order_list items
+    order_list_delete_all()
+    return redirect('/order_page')
 
 
+# Function: Delete all the records at the order_list table
+def order_list_delete_all():
+    Order_list.objects.all().delete()
+    return
+
+# Process: Clear order_list Items
+def clear_all_order_list_process(request):
+    order_list_delete_all()
+    return redirect('/order_page')
+
+# Process: Logout
+def logout_process(request):
+    request.session.flush()
+    return redirect('/')
+
+# Function : Check Session
+def check_session(request):
+    if request.session.has_key('user') == True:
+        user_session = User.objects.get(id=request.session['user'])
+    else:
+        user_session = False
+    return user_session
